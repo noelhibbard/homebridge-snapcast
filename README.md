3634ff178f993cd2a300ba202d00fd328d8fea0e
Noel Hibbard <NoelH@jaru.com>
2020-06-23 11:40:05 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Homebridge-snapcast
+
+`homebridge-snapcast` is a Homebridge plugin for controlling Snapcast clients. HomeKit has an official Service.Speaker but Apple hasn't implamented this service in the Home App so the workaround is to use Service.Lightbulb and use the brightness to control the volume.
+
+### Example plugin configuration:
+```
+{
+    "accessory":"Snapcast",
+    "name":"Bathroom",
+    "groupname":"Whole Home",
+    "serverUrl":"http://localhost:1780/jsonrpc"
+}
+```
+
+### Option Descriptions:
+
+```
+name  : <string> name of the snapcast client (Ex: Bathroom)
+groupname   : <string> name of the group that the snapcast client is located (Ex: Whole Home)
+serverUrl   : <string> URL to Snapcast JSON-RPC server (Ex: http://localhost:1780/jsonrpc)
+```
---
+++ index.js
@@ -0,0 +1,103 @@
+// HomeKit provides an official Service.Speaker type but Apple didn't bother implamenting it in the Home App
+// so I am forced use Service.Lightbulb and rether than volume I use brightness.
+
+const request = require('request')
+
+let Service, Characteristic
+
+module.exports = (homebridge) => {
+    Service = homebridge.hap.Service
+    Characteristic = homebridge.hap.Characteristic
+    homebridge.registerAccessory('homebridge-snapcast', 'Snapcast', DimmerAccessory)
+}
+
+class DimmerAccessory {
+    constructor(log, config) {
+        this.log = log
+        this.config = config
+        this.service = new Service.Lightbulb(this.config.name)
+        this.id = '00:00:00:00:00:00'
+        this.muted = true
+        this.volume = 0
+    }
+
+    getServices() {
+        const informationService = new Service.AccessoryInformation()
+            .setCharacteristic(Characteristic.Manufacturer, 'Noel Hibbard')
+            .setCharacteristic(Characteristic.Model, 'Snapcast')
+            .setCharacteristic(Characteristic.SerialNumber, 'Snapcast')
+
+        this.service.getCharacteristic(Characteristic.On)
+            .on('get', this.getOnCharacteristicHandler.bind(this))
+            .on('set', this.setOnCharacteristicHandler.bind(this))
+
+        this.service.getCharacteristic(Characteristic.Brightness)
+            .on('get', this.getBrightness.bind(this))
+            .on('set', this.setBrightness.bind(this))
+
+        return [informationService, this.service]
+    }
+
+    getBrightness(callback) {
+        request(this.config.serverUrl, {
+            method: 'POST', json: { id: 1, jsonrpc: '2.0', method: 'Server.GetStatus' }
+        }, (err, resp, json) => {
+            if (err) {
+                this.log(err)
+            }
+            json.result.server.groups.forEach(group => {
+                if (group.name == this.config.groupname) {
+                    group.clients.forEach(client => {
+                        if (client.config.name == this.config.name) {
+                            this.id = client.id
+                            this.volume = client.config.volume.percent
+                        }
+                    })
+                }
+            })
+            callback(null, this.volume)
+        })
+    }
+
+    setBrightness(value, callback) {
+        request(this.config.serverUrl, {
+            method: 'POST', json: { id: 1, jsonrpc: '2.0', method: 'Client.SetVolume', params: { id: this.id, volume: { percent: value } } }
+        }, (err, resp, json) => {
+            if (err) {
+                this.log(err)
+            }
+            callback(null)
+        })
+    }
+
+    getOnCharacteristicHandler(callback) {
+        request(this.config.serverUrl, {
+            method: 'POST', json: { id: 1, jsonrpc: '2.0', method: 'Server.GetStatus' }
+        }, (err, resp, json) => {
+            if (err) {
+                this.log(err)
+            }
+            json.result.server.groups.forEach(group => {
+                if (group.name == this.config.groupname) {
+                    json.result.server.groups[0].clients.forEach(client => {
+                        if (client.config.name == this.config.name) {
+                            this.muted = client.config.volume.muted
+                        }
+                    })
+                }
+            })
+            callback(null, !this.muted)
+        })
+    }
+
+    setOnCharacteristicHandler(value, callback) {
+        request(this.config.serverUrl, {
+            method: 'POST', json: { id: 1, jsonrpc: '2.0', method: 'Client.SetVolume', params: { id: this.id, volume: { muted: !value } } }
+        }, (err, resp, json) => {
+            if (err) {
+                this.log(err)
+            }
+            callback(null)
+        })
+    }
+}
